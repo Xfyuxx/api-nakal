063583c98e02926bd62e084fdf417959a68a35b6
Xfyuxx <89505417+Xfyuxx@users.noreply.github.com>
2022-06-19 20:50:08 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-	res.status(200).send('Hello world from, home page')
+	res.render("./src/view/home/");
 });
 
 // Api routes
