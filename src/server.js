05114f1ce6fecb877e290adc7cf727fc51d2c205
Xfyuxx <89505417+Xfyuxx@users.noreply.github.com>
2022-06-19 16:40:31 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const app = require('./app');
-const settings = require('./config/settings');
+//const settings = require('./config/settings');
 
-app.listen(settings.port, () => {
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`Test Server Is Ready ${process.env.PORT || 3000}`)
+});
+
+
+/*app.listen(settings.port, () => {
 	console.log('Test Server Is Ready')
 	console.log(`Application running in port : ${settings.port}`);
 	
 	
-});
+});*/
  
 
