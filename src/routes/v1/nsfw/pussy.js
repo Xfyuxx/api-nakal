ae128f3ef201145601510dcbb15e4a59764c0d91
Xfyuxx <89505417+Xfyuxx@users.noreply.github.com>
2022-06-19 17:23:14 +0700

    Create pussy.js

---
+++ src/routes/v1/nsfw/pussy.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const { RandomPHUB } = require('discord-phub');
+const nsfw = new RandomPHUB(unique = true);
+
+router.get('/', (req, res) => {
+   
+    const { data } = nsfw.getRandomInCategory('pussy', "jpg");
+
+    res.status(200).send({
+        url: data.url,
+        
+    });
+});
+
+module.exports = router;
